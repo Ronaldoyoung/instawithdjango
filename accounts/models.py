dc3f0ebc5973307a286b928cb2a7c5e67d6fc80b
ronaldoyoung <ronaldoyoung@gmail.com>
2021-09-08 20:25:43 +0900

    프로필 수정1

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
 from django.conf import settings
+from django.core.validators import RegexValidator
 from django.db import models
 from django.template.loader import render_to_string
 
 # Create your models here.
 
-
 class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성"
+        FEMALE = "F", "여성"
+
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=13, blank=True, validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")])
+    gender = models.CharField(max_length=1, blank=True, choices=GenderChoices.choices)
 
     def send_welcome_email(self):
 
