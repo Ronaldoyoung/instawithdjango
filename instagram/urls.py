9795c9852cc2ce376f7aac2ca9115e9514dddf99
ronaldoyoung <ronaldoyoung@gmail.com>
2021-09-11 14:18:12 +0900

    유저 페이지 구현

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
-app_name = 'instagram' # app name 을 지정하게 되면 url reverse 의 namespace 역할을 하게 된다.
+
+app_name = 'instagram'  # app name 을 지정하게 되면 url reverse 의 namespace 역할을 하게 된다.
 
 urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    re_path(r'^(?P<username>[\w.@+-]+)/$', views.user_page, name='user_page')
 ]
