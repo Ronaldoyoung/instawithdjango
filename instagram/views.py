1eb4d18e11bbd99be7bcdd097dd79e467754434c
ronaldoyoung <ronaldoyoung@gmail.com>
2021-09-12 00:00:30 +0900

    Timeline 포스팅 목록 조건에 날짜 범위 저장

--- instagram/views.py
+++ instagram/views.py
@@ -1,17 +1,23 @@
+from datetime import timedelta
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
+from django.utils import timezone
 from instagram.forms import PostForm
 from instagram.models import Tag, Post
 
 @login_required()
 def index(request):
+    timesince = timezone.now() - timedelta(days=3)
     post_list = Post.objects.all()\
         .filter(
             Q(author=request.user) |
             Q(author__in=request.user.following_set.all())
+        )\
+        .filter(
+            created_at__gte=timesince   # greater then equal
         )
 
     suggested_user_list = get_user_model().objects.all()\
