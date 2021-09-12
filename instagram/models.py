68212e03ea936ca9b63818d7e16db108055f3578
ronaldoyoung <ronaldoyoung@gmail.com>
2021-09-12 09:30:36 +0900

    포스팅 좋아요-취소 구현

--- instagram/models.py
+++ instagram/models.py
@@ -10,13 +10,18 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
+#  user
+#  -> Post.objects.filter(author=user)
+#  -> user.post_set.all()
+
 
 class Post(BaseModel):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='my_post_set', on_delete=models.CASCADE)
     photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
     caption = models.CharField(max_length=500)
     tag_set = models.ManyToManyField('Tag', blank=True)
     location = models.CharField(max_length=100)
+    like_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='like_post_set')
 
     def __str__(self):
         return self.caption
@@ -32,9 +37,20 @@ class Post(BaseModel):
     def get_absolute_url(self):
         return reverse("instagram:post_detail", args=[self.pk])
 
+    def is_like_user(self, user):
+        return self.like_user_set.filter(pk=user.pk).exists()
+
+    class Meta:
+        ordering = ['-id']
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
+
+
+# Like Table 을 따로 만들 수 도 있다. 추가적인 정보가 필요할 경우에는 이런 식으로 만들어도 된다.
+# class LikeUser(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
